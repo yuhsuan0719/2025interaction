46b588a08320e459eccd301de4ac3190d2973010
yuhsuan0719 <145088360+yuhsuan0719@users.noreply.github.com>
2025-10-27 16:57:51 +0800

    week08

---
+++ week08/week08_Arduino_G5_C5/week08_Arduino_G5_C5.ino
@@ -0,0 +1,29 @@
+//week08_Arduino_G5_C5
+void setup(){
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);//發聲音的8
+  tone(8, 784, 100);//聲音so
+  delay(100);
+  tone(8, 523, 100);//聲音do
+  delay(100);
+}
+void serialEvent(){
+  while (Serial.available()){
+    char now = Serial.read();
+    if (now=='R') {
+      tone(8, 784, 100);//聲音so
+      delay(100);
+      tone(8, 523, 100);//聲音do
+      delay(100);
+    }else {
+     tone(8, 523, 100);//聲音so
+     delay(100);
+     tone(8, 784, 100);//聲音do
+     delay(100);
+    }
+  }
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
